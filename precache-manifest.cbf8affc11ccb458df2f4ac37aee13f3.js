ab0722e21a1493f49b6d49d50bb10cf0293bb473
ritikvashistha <ritik@sutratechlabs.com>
2020-12-12 15:20:15 +0530

    Updates

---
+++ precache-manifest.cbf8affc11ccb458df2f4ac37aee13f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985e34ad794e2872785c83dd02102580",
+    "url": "/contactbook/index.html"
+  },
+  {
+    "revision": "f77b27502860fd6b652f",
+    "url": "/contactbook/static/css/main.5d6b07b6.chunk.css"
+  },
+  {
+    "revision": "89d15acf615d7791bf2f",
+    "url": "/contactbook/static/js/2.db5faf41.chunk.js"
+  },
+  {
+    "revision": "d0c7295dade8792d4ec795e8fb19eb02",
+    "url": "/contactbook/static/js/2.db5faf41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f77b27502860fd6b652f",
+    "url": "/contactbook/static/js/main.a88c0f2f.chunk.js"
+  },
+  {
+    "revision": "8ff86701b87313444206",
+    "url": "/contactbook/static/js/runtime-main.7288606f.js"
+  }
+]);
